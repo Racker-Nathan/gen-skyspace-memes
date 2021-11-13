37e9b31dc27cac1685d32ab6cffd8801f52ee73b
Nathan Fleury <Nathan.Fleury@rackspace.com>
2021-11-13 15:33:28 -0600

    update cookie used in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ AWS_REGION = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/pla
 def fetch_meme_string():
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': '__utmc=71379083; iflipsess=palvmg7qnnf2uoie0vt8hed0kj; __utma=71379083.734663800.1635440230.1636492023.1636653592.7; __utmz=71379083.1636653592.7.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=71379083.2.9.1636653592',
+        'cookie': '__utma=71379083.912587323.1636665583.1636665583.1636838844.2; __utmz=71379083.1636665583.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __gads=ID=b302e7237bf740e0-223c03924acc0027:T=1636665708:RT=1636665708:S=ALNI_MYcrw7Y91nLLhhtXAYFlkdcyvS0ZQ; iflipsess=l554m04t8khi5r4vjoe8cdm3m8; __utmb=71379083.3.10.1636838844; __utmc=71379083; __utmt=1; _ga=GA1.2.912587323.1636665583; _gid=GA1.2.454347243.1636838845; _gat_gtag_UA_164290288_48=1; _fbp=fb.1.1636838846210.1874540950',
     }
 
     post_request = 'use_openai=0&meme_id=563423&init_text=&__tok=gvH6kpgsZYvGnuSeEHSdxnEZr%2B2dmZDBrMOfwxUOo9s%3D&__cookie_enabled=1'
